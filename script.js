17f0a0e8959c447155c1ab298b35a69fa5090cb2
vanta blanta <vantablanta@gmail.com>
2022-03-15 15:25:21 +0300

    literal notation

---
+++ script.js
@@ -0,0 +1,18 @@
+/*let flower = {
+    name: "daisy",
+    height: 5, 
+    color: "pink"
+}
+flower.color = "blue"
+flower['height'] = 10
+flower['pollinators'] = ["bees", "butterfly", "worms"]
+flower['pollinators'].push("human")
+
+
+flower.grow = function grow(){
+    return flower.height + 5
+}
+
+console.log(flower)
+*/
+
